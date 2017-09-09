a0f65164236a1d23136cb2dc186bc3405268e78b
CKD <phamduyanh@gmail.com>
2017-09-09 10:44:20 +0700

    Add files via upload

---
+++ Arduino_LED_Status_multi_Flashes.ino
@@ -0,0 +1,40 @@
+/***************************************************************
+ * Pham Duy Anh - CKD - phamduyanh@gmail.com
+ * Create: 08-09-2017
+ * Update: 09-09-2017
+ * Forum:  http://forum.cncprovn.com/forums/80-Arduino
+ * 
+ * This function will flash a LED with cycles
+ *                                                         state             (stat flashing led, HIGHT or LOW);
+ *             *     *     *<----------------------------- timeBlink         (the number of times the leds flash in one cycle)
+ *          ->| |<---------------------------------------- timeLED           (time led flash, ms)
+ *          ->| |                                      |<- intervalLEDStatus (time of a cycle, ms)
+ * Led on      _     _     _
+ * Led off  __| |___| |___| |__________________________|
+ * 
+ * Example:
+ *   void loop()
+ *   {
+ *     LEDStatusFlashes(13, 2, 2000, 1);
+ *   }
+ * 
+ * Attention:
+ *   The cycle will not be accurate if the loop() time is too large
+ **************************************************************/
+ 
+#define portLED PORTB
+#define pinLED  5       //pin 13
+#define portChk PORTB
+#define pinChk  4       //pin 12
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  bitWrite(portChk, pinChk, !bitRead(portChk, pinChk));
+  LEDStatusFlashes(13, 1, 2000, 1);
+}
---
+++ _LEDStatusFlashes.ino
@@ -0,0 +1,61 @@
+/***************************************************************
+ * Pham Duy Anh - CKD - phamduyanh@gmail.com
+ * Create: 08-09-2017
+ * Update: 09-09-2017
+ * Forum:  http://forum.cncprovn.com/forums/80-Arduino
+ * 
+ * This function will flash a LED with cycles
+ *                                                         state             (stat flashing led, HIGHT or LOW);
+ *             *     *     *<----------------------------- timeBlink         (the number of times the leds flash in one cycle)
+ *          ->| |<---------------------------------------- timeLED           (time led flash, ms)
+ *          ->| |                                      |<- intervalLEDStatus (time of a cycle, ms)
+ * Led on      _     _     _
+ * Led off  __| |___| |___| |__________________________|
+ * 
+ * Example:
+ *   void loop()
+ *   {
+ *     LEDStatusFlashes(13, 2, 2000, 1);
+ *   }
+ * 
+ * Attention:
+ *   The cycle will not be accurate if the loop() time is too large
+ **************************************************************/
+
+void LEDStatusFlashes(byte pin, byte timeBlink, unsigned int intervalLEDStatus, byte state)
+{
+  #define timeLED 10
+
+  static long timeLEDStatus;
+  static byte timeBlinked;
+  static bool ledStatus;
+
+  pinMode(pin, OUTPUT);
+
+  if (millis() - timeLEDStatus > intervalLEDStatus)
+  {
+    ledStatus     = true;
+    timeBlinked   = 1;
+    timeLEDStatus = millis();
+  }
+
+  long checkTime = millis() - (timeLEDStatus + timeBlinked*timeLED*30);
+
+  if (ledStatus && (checkTime > 0))
+  {
+    timeBlinked++;
+    if (timeBlinked > timeBlink)
+    {
+      timeBlinked = 0;
+      ledStatus   = false;
+    }
+    digitalWrite(pin, state);
+    delay(timeLED);
+    digitalWrite(pin, !state);
+  }
+  else
+  {
+    digitalWrite(pin, !state);
+  }
+}
+
